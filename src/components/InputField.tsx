693c906252078bd971b069339cc1b11b791ed52b
yunitjung <tjungyuni@gmail.com>
2021-07-06 11:40:13 +0700

    create input field component

---
+++ src/components/InputField.tsx
@@ -0,0 +1,20 @@
+import { FormControl, FormLabel, Input, FormErrorMessage } from '@chakra-ui/react';
+import { useField } from 'formik';
+import React, { InputHTMLAttributes } from 'react';
+
+type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
+    name: string;
+    label: string;
+    placeholder: string;
+}
+
+export const InputField: React.FC<InputFieldProps> = (props) => {
+    const [field, {error}] = useField(props);
+    return (
+        <FormControl isInvalid={!!error}>
+            <FormLabel htmlFor={field.name}>{props.label}</FormLabel>
+            <Input {...field} id={field.name} placeholder={props.placeholder} />
+            <FormErrorMessage>{error}</FormErrorMessage>
+        </FormControl>
+    );
+}
