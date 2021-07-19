2b454c6c0a155f4ddb19bf003b0a3ee4c0308a4f
yunitjung <tjungyuni@gmail.com>
2021-07-19 20:31:01 +0700

    add error handling and redirect after register

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,30 +1,46 @@
-import React from 'react'
-import {Form, Formik} from 'formik'
-import { FormControl, FormLabel, Input, FormErrorMessage } from '@chakra-ui/react';
-import { Wrapper } from '../components/Wrapper';
+import React from "react";
+import { Form, Formik } from "formik";
+import { Box, Button } from "@chakra-ui/react";
+import { Wrapper } from "../components/Wrapper";
+import { InputField } from "../components/InputField";
+import { useRegisterMutation } from "../generated/graphql";
+import { toErrorMap } from "../utils/toErrorMap";
+import { useRouter } from "next/router";
 
 interface registerProps {}
 
 const Register: React.FC<registerProps> = ({}) => {
-    return (
-        <Wrapper>
-        <Formik 
-            initialValues={{ username: "", password: ""}}
-            onSubmit={(values) => {
-                console.log(values)
-            }}>
-            {({ values, handleChange }) => (
-                <Form>
-                    <FormControl>
-                        <FormLabel htmlFor="username">Username</FormLabel>
-                        <Input value={values.username} onChange={handleChange} id="username" placeholder="username" />
-                        {/* <FormErrorMessage>{form.errors.name}</FormErrorMessage> */}
-                    </FormControl>
-                </Form>
-            )}
-        </Formik>
-        </Wrapper>    
-    );
-}
+  const router = useRouter();
+  const [, register] = useRegisterMutation();
+  return (
+    <Wrapper>
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={async (values, {setErrors}) => {
+          const response = await register(values);
+          console.log(response)
+          if(response.data?.register.errors){
+            setErrors(toErrorMap(response.data.register.errors));
+          }else if(response.data?.register.user){
+            console.log(response.data.register)
+            router.push("/");
+          }
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField name="username" placeholder="username" label="Username" />
+            <Box mt={4}>
+              <InputField name="password" placeholder="password" label="Password" type="password" />
+            </Box>
+            <Button mt={4} type="submit" isLoading={isSubmitting} colorScheme="teal">
+              Register
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
 
-export default Register
+export default Register;
