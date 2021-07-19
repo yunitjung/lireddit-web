98c441b5771dadc389322fac4d94716d3864e3b8
yunitjung <tjungyuni@gmail.com>
2021-07-19 20:29:23 +0700

    update input field with the rest of props

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -5,15 +5,14 @@ import React, { InputHTMLAttributes } from 'react';
 type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
     name: string;
     label: string;
-    placeholder: string;
 }
 
-export const InputField: React.FC<InputFieldProps> = (props) => {
+export const InputField: React.FC<InputFieldProps> = ({label, size:_, ...props}) => {
     const [field, {error}] = useField(props);
     return (
         <FormControl isInvalid={!!error}>
-            <FormLabel htmlFor={field.name}>{props.label}</FormLabel>
-            <Input {...field} id={field.name} placeholder={props.placeholder} />
+            <FormLabel htmlFor={field.name}>{label}</FormLabel>
+            <Input {...field} {...props} id={field.name}/>
             <FormErrorMessage>{error}</FormErrorMessage>
         </FormControl>
     );
