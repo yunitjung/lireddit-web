fe8dba2563f1e4770e66ffb3bd27abd40d1a2a57
yunitjung <tjungyuni@gmail.com>
2021-07-19 20:57:14 +0700

    add login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -3,24 +3,22 @@ import { Form, Formik } from "formik";
 import { Box, Button } from "@chakra-ui/react";
 import { Wrapper } from "../components/Wrapper";
 import { InputField } from "../components/InputField";
-import { useRegisterMutation } from "../generated/graphql";
+import { useLoginMutation } from "../generated/graphql";
 import { toErrorMap } from "../utils/toErrorMap";
 import { useRouter } from "next/router";
 
-interface registerProps {}
-
-const Register: React.FC<registerProps> = ({}) => {
+const Login: React.FC<{}> = ({}) => {
   const router = useRouter();
-  const [, register] = useRegisterMutation();
+  const [, login] = useLoginMutation();
   return (
     <Wrapper>
       <Formik
         initialValues={{ username: "", password: "" }}
         onSubmit={ async (values, {setErrors}) => {
-          const response = await register(values);
-          if(response.data?.register.errors){
-            setErrors(toErrorMap(response.data.register.errors));
-          }else if(response.data?.register.user){
+          const response = await login({ options: values});
+          if(response.data?.login.errors){
+            setErrors(toErrorMap(response.data.login.errors));
+          }else if(response.data?.login.user){
             router.push("/");
           }
         }}
@@ -41,4 +39,4 @@ const Register: React.FC<registerProps> = ({}) => {
   );
 };
 
-export default Register;
+export default Login;
