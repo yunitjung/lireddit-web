0785d211088b49ce9147aea350af18a856f8f9f9
yunitjung <tjungyuni@gmail.com>
2021-07-21 23:07:25 +0700

    change index to render data server sie

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
 import { NavBar } from "../components/NavBar";
+import { withUrqlClient } from "next-urql";
+import { createUrqlClient } from "../utils/createUrlClient";
+import { usePostsQuery } from "../generated/graphql";
 
-const Index = () => (
-  <>
-  <NavBar/>
-    <div>Hello world</div>
-  </>
-)
+const Index = () => {
+  const [{ data }] = usePostsQuery();
 
-export default Index
+  return (
+    <>
+      <NavBar />
+      <div>Hello world</div>
+      <hr></hr>
+      {!data ? <div>loading ...</div> : data.posts.map((p) => <div key={p.id}>{p.title}</div>)}
+    </>
+  );
+};
+
+export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
