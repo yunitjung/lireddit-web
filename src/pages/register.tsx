45dde38f0d37f8c52a406615b7b091730171a917
yunitjung <tjungyuni@gmail.com>
2021-07-22 21:15:21 +0700

    add field email to register form

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -17,9 +17,9 @@ const Register: React.FC<registerProps> = ({}) => {
   return (
     <Wrapper>
       <Formik
-        initialValues={{ username: "", password: "" }}
+        initialValues={{ username: "", password: "", email: "" }}
         onSubmit={async (values, { setErrors }) => {
-          const response = await register(values);
+          const response = await register({ options: values });
           if (response.data?.register.errors) {
             setErrors(toErrorMap(response.data.register.errors));
           } else if (response.data?.register.user) {
@@ -30,6 +30,9 @@ const Register: React.FC<registerProps> = ({}) => {
         {({ isSubmitting }) => (
           <Form>
             <InputField name="username" placeholder="username" label="Username" />
+            <Box mt={4}>
+              <InputField name="email" placeholder="email" label="Email" type="email" />
+            </Box>
             <Box mt={4}>
               <InputField name="password" placeholder="password" label="Password" type="password" />
             </Box>
