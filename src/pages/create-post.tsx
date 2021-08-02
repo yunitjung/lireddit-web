e8581d112595350bdc3a56744cabfca8efc63817
yunitjung <tjungyuni@gmail.com>
2021-08-02 22:19:56 +0700

    add create post form

---
+++ src/pages/create-post.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Form, Formik } from "formik";
+import { Box, Button } from "@chakra-ui/react";
+import { Wrapper } from "../components/Wrapper";
+import { InputField } from "../components/InputField";
+import { useCreatePostMutation } from "../generated/graphql";
+import { useRouter } from "next/router";
+import { withUrqlClient } from "next-urql";
+import { createUrqlClient } from "../utils/createUrqlClient";
+import { useIsAuth } from "../utils/useIsAuth";
+
+export const CreatePost: React.FC<{}> = ({}) => {
+  const router = useRouter();
+  useIsAuth();
+  const [, createPost] = useCreatePostMutation();
+  return (
+    <Wrapper>
+      <Formik
+        initialValues={{ title: "", text: "" }}
+        onSubmit={async (values) => {
+          const { error } = await createPost({ input: values });
+          if (!error) {
+            router.push("/");
+          }
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField name="title" placeholder="Title" label="title" />
+            <Box mt={4}>
+              <InputField
+                textarea
+                name="text"
+                placeholder="text..."
+                label="Body"
+              />
+            </Box>
+            <Button
+              mt={4}
+              type="submit"
+              isLoading={isSubmitting}
+              colorScheme="teal"
+            >
+              Create Post
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
+
+export default withUrqlClient(createUrqlClient)(CreatePost);
