ceb90b02bf37814982684d76a2e86eee72d19406
yunitjung <tjungyuni@gmail.com>
2021-08-02 22:19:09 +0700

    fix input field issue

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -3,17 +3,22 @@ import {
   FormLabel,
   Input,
   FormErrorMessage,
+  Textarea,
 } from "@chakra-ui/react";
 import { useField } from "formik";
 import React, { InputHTMLAttributes } from "react";
 
-type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
+type InputFieldProps = InputHTMLAttributes<
+  HTMLInputElement | HTMLTextAreaElement
+> & {
   name: string;
   label: string;
+  textarea?: boolean;
 };
 
 export const InputField: React.FC<InputFieldProps> = ({
   label,
+  textarea,
   size: _,
   ...props
 }) => {
@@ -21,7 +26,11 @@ export const InputField: React.FC<InputFieldProps> = ({
   return (
     <FormControl isInvalid={!!error}>
       <FormLabel htmlFor={field.name}>{label}</FormLabel>
-      <Input {...field} {...props} id={field.name} />
+      {textarea ? (
+        <Textarea {...field} {...props} id={field.name} />
+      ) : (
+        <Input {...field} {...props} id={field.name} />
+      )}
       <FormErrorMessage>{error}</FormErrorMessage>
     </FormControl>
   );
