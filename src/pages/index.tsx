d58bdbc385d7ed4b54fdc23171ab7340308b3c4a
yunitjung <tjungyuni@gmail.com>
2021-08-03 21:36:14 +0700

    styling cards

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,23 +3,33 @@ import { createUrqlClient } from "../utils/createUrqlClient";
 import { usePostsQuery } from "../generated/graphql";
 import { Layout } from "../components/Layout";
 import NextLink from "next/link";
-import { Link } from "@chakra-ui/react";
+import { Link, Stack, Box, Heading, Text } from "@chakra-ui/react";
 
 const Index = () => {
-  const [{ data }] = usePostsQuery();
+  const [{ data }] = usePostsQuery({
+    variables: {
+      limit: 10,
+    },
+  });
 
   return (
     <Layout>
       <NextLink href="/create-post">
         <Link>Create Post</Link>
       </NextLink>
-      <br />
-      <div>Hello world</div>
       <hr></hr>
+      <br />
       {!data ? (
         <div>loading ...</div>
       ) : (
-        data.posts.map((p) => <div key={p.id}>{p.title}</div>)
+        <Stack spacing={8}>
+          {data.posts.map((p) => (
+            <Box p={5} key={p.id} shadow="md" borderWidth="1px">
+              <Heading fontSize="xl">{p.title}</Heading>
+              <Text mt={4}>{p.textSnippet}</Text>
+            </Box>
+          ))}
+        </Stack>
       )}
     </Layout>
   );
