efa6519446020aa5e4b73ad7b9e3103b5658da7a
yunitjung <tjungyuni@gmail.com>
2021-08-04 23:46:46 +0700

    show button load more for pagination with cursor

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,27 +3,45 @@ import { createUrqlClient } from "../utils/createUrqlClient";
 import { usePostsQuery } from "../generated/graphql";
 import { Layout } from "../components/Layout";
 import NextLink from "next/link";
-import { Link, Stack, Box, Heading, Text } from "@chakra-ui/react";
+import {
+  Link,
+  Stack,
+  Box,
+  Heading,
+  Text,
+  Flex,
+  Button,
+} from "@chakra-ui/react";
+import { useState } from "react";
 
 const Index = () => {
-  const [{ data }] = usePostsQuery({
-    variables: {
-      limit: 10,
-    },
+  const [variables, setVariables] = useState({
+    limit: 33,
+    cursor: null as null | string,
   });
+
+  const [{ data, fetching }] = usePostsQuery({
+    variables,
+  });
+
+  if (!fetching && !data) {
+    return <div>failed to retrieve your data!</div>;
+  }
 
   return (
     <Layout>
-      <NextLink href="/create-post">
-        <Link>Create Post</Link>
-      </NextLink>
-      <hr></hr>
+      <Flex align="center">
+        <Heading>LiReddit</Heading>
+        <NextLink href="/create-post">
+          <Link ml="auto">Create Post</Link>
+        </NextLink>
+      </Flex>
       <br />
-      {!data ? (
+      {!data && fetching ? (
         <div>loading ...</div>
       ) : (
         <Stack spacing={8}>
-          {data.posts.map((p) => (
+          {data!.posts.posts.map((p) => (
             <Box p={5} key={p.id} shadow="md" borderWidth="1px">
               <Heading fontSize="xl">{p.title}</Heading>
               <Text mt={4}>{p.textSnippet}</Text>
@@ -31,6 +49,26 @@ const Index = () => {
           ))}
         </Stack>
       )}
+      {data && data.posts.hasMore ? (
+        <Flex>
+          <Button
+            onClick={() => {
+              setVariables({
+                limit: variables.limit,
+                cursor:
+                  data.posts.posts[data.posts.posts.length - 1].createdAt,
+              });
+
+              console.log(data);
+            }}
+            isLoading={fetching}
+            m="auto"
+            my={8}
+          >
+            load more
+          </Button>
+        </Flex>
+      ) : null}
     </Layout>
   );
 };
