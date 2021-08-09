b66c3471f5b519d2d224e772893d804c15aca756
yunitjung <tjungyuni@gmail.com>
2021-08-09 21:34:35 +0700

    add use get int id hook

---
+++ src/utils/useGetPostFromUrl.ts
@@ -0,0 +1,12 @@
+import { useGetIntId } from "./useGetIntId";
+import { usePostQuery } from "../generated/graphql";
+
+export const useGetPostFromUrl = () => {
+  const intId = useGetIntId();
+  return usePostQuery({
+    pause: intId === -1,
+    variables: {
+      id: intId,
+    },
+  });
+};
