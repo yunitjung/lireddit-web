e8d975e3e9d2173340ac243e918362479eb7acae
yunitjung <tjungyuni@gmail.com>
2021-08-09 22:24:03 +0700

    show error message on index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,12 +22,17 @@ const Index = () => {
     cursor: null as null | string,
   });
 
-  const [{ data, fetching }] = usePostsQuery({
+  const [{ data, error, fetching }] = usePostsQuery({
     variables,
   });
 
   if (!fetching && !data) {
-    return <div>failed to retrieve your data!</div>;
+    return (
+      <div>
+        <div>failed to retrieve your data!</div>;
+        <div>{error?.message}</div>
+      </div>
+    );
   }
 
   return (
